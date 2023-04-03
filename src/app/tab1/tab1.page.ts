87fbb4aebac6bf4b1e356376b9d1dfebdd1df88d
Adrián Jáimez <adroloja@gmail.com>
2023-04-03 17:27:12 +0200

    Fin seccion 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Console } from 'console';
 
 @Component({
   selector: 'app-tab1',
@@ -9,6 +10,20 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   standalone: true,
   imports: [IonicModule, ExploreContainerComponent],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
   constructor() {}
+  ngOnInit(): void {
+    
+    let numero = 10;
+    //debugger;       // Es una instruccion para que se pare en ese momento
+
+    numero = 20;
+
+    numero = numero + 10;
+
+    numero = 0;
+
+    console.log(numero);
+    
+  }
 }
